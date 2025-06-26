eb88a2b34baaf73e14bcf20a2e125ed03cd1082e
Andrei Soares <andreicarvalho1370@gmail.com>
2025-06-26 13:47:54 -0300

    styles dropdown menu

--- my-portfolio/components/Header.tsx
+++ my-portfolio/components/Header.tsx
@@ -1,15 +1,15 @@
-// import Link from "next/link";
+import Link from "next/link";
 import { DropdownMenuHeader } from "./dropdown-menu";
 
 export default function Header() {
 return (
-<header className="fixed p-3 w-full">
+<header className="fixed p-3 w-full items-center border-b border-gray-300">
   <div className="flex justify-between">
-   <h1 className="max-sm:text-lg font-bold">Andrei Soares</h1>
+   <Link href={"/"} className="max-sm:text-lg font-bold text-[#2D2D2D]">Andrei Soares</Link>
    <div className="flex gap-2">
      <DropdownMenuHeader/>
    </div>
   </div>
 </header>
-);
+)
 }

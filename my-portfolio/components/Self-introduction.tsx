dacc66c18f21886a4bee04cf466ba5f61d24fd9a
Andrei Soares <andreicarvalho1370@gmail.com>
2025-06-27 14:14:49 -0300

    add "view my work" button

--- my-portfolio/components/Self-introduction.tsx
+++ my-portfolio/components/Self-introduction.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function SelfIntroduction() {
   return (
@@ -9,7 +10,9 @@ export default function SelfIntroduction() {
       </div>
       <div>
         <h1 className="text-[#2D2D2D] font-bold text-4xl pb-7">Desenvolvedor Front-End & <span className="text-[#990000]">Designer UI/UX</span></h1>
-        <p className="text-[#2D2D2D] opacity-80 text-lg font-[Arial]">Sou um desenvolvedor Front-End e Designer UI/UX, estou à 1 ano meio programando, criando projetos pessoais para aprimorar minhas habilidades como programador. Procuro oportunidades para entrar no mercado de trabalho e me desenvolver profissionalmente.</p>
+        <p className="text-[#2D2D2D] opacity-80 text-lg font-[Arial] pb-6">Sou um desenvolvedor Front-End e Designer UI/UX, estou à 1 ano meio programando, criando projetos pessoais para aprimorar minhas habilidades como programador. Procuro oportunidades para entrar no mercado de trabalho e me desenvolver profissionalmente.</p>
+        <Link href="/"><button className="flex w-full gap-2 justify-center items-center bg-[#CC0000] text-white p-2 rounded-md">Ver meu trabalho <Image src="/arrow-right.svg" alt="arrow right icon" width={12} height={12}/></button></Link>
+        <button></button>
       </div>
     </div>
   )

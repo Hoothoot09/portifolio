b66db5d037720ba6bb341e81e0f84a59bbfe7ab5
Andrei Soares <andreicarvalho1370@gmail.com>
2025-06-30 12:04:20 -0300

    add "About me" section

---
+++ my-portfolio/components/About-me.tsx
@@ -0,0 +1,12 @@
+export default function AboutMe() {
+  return (
+    <div className="flex flex-col w-full p-3 mt-6 justify-center items-center">
+     <div className="mb-5">
+      <h1 className="text-3xl text-[#2D2D2D] font-bold">Sobre Mim</h1>
+     </div>
+     <div>
+      <p className="max-w-3xl text-xl text-[#2D2D2D] opacity-80 mx-auto">Estou à 1 ano e meio estudando sobre linguagens de Front-End, aprimorando e desenvolvendo minhas habilidades.</p>
+     </div>
+    </div>
+  );
+}

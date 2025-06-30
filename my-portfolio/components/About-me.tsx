9f6ead0bd1b070b1de091c43a591f324e70561cd
Andrei Soares <andreicarvalho1370@gmail.com>
2025-06-30 13:39:54 -0300

    add personal skills

--- my-portfolio/components/About-me.tsx
+++ my-portfolio/components/About-me.tsx
@@ -5,7 +5,15 @@ export default function AboutMe() {
       <h1 className="text-3xl text-[#2D2D2D] font-bold">Sobre Mim</h1>
      </div>
      <div>
-      <p className="max-w-3xl text-xl text-[#2D2D2D] opacity-80 mx-auto">Estou à 1 ano e meio estudando sobre linguagens de Front-End, aprimorando e desenvolvendo minhas habilidades.</p>
+      <p className="max-w-xl text-xl text-[#2D2D2D] opacity-80 mx-auto">Estou à 1 ano e meio estudando sobre linguagens de Front-End, aprimorando e desenvolvendo minhas habilidades.</p>
+     </div>
+     <div className="flex flex-col gap-4 mt-6 border border-[#d2d2d2] p-4 rounded-lg bg-white shadow-md max-w-3xl">
+      <div>
+       <h3 className="text-2xl font-bold">Desenvolvimento Front-End</h3>
+      </div>
+      <div>
+       <p className="text-lg text-[#2D2D2D] opacity-80">Especialista em React, Next.js, TypeScript e frameworks modernos.</p>
+      </div>
      </div>
     </div>
   );

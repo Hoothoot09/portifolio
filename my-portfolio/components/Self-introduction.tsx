2873210e09b0c4227e41c32525449e15755d8f52
Andrei Soares <andreicarvalho1370@gmail.com>
2025-06-30 10:50:46 -0300

    add envelope button

--- my-portfolio/components/Self-introduction.tsx
+++ my-portfolio/components/Self-introduction.tsx
@@ -8,15 +8,15 @@ export default function SelfIntroduction() {
        <Image src="/localization.svg" alt="localization icon" width={12} height={12}/>
        <span className="text-[#2D2D2D]">Salvador, BA</span>
       </div>
-      <div className="">
+      <div>
         <h1 className="text-[#2D2D2D] font-bold text-4xl pb-7">Desenvolvedor Front-End & <span className="text-[#990000]">Designer UI/UX</span></h1>
         <p className="text-[#2D2D2D] opacity-80 text-lg font-[Arial] pb-6">Sou um desenvolvedor Front-End e Designer UI/UX, estou à 1 ano meio programando, criando projetos pessoais para aprimorar minhas habilidades como programador. Procuro oportunidades para entrar no mercado de trabalho e me desenvolver profissionalmente.</p>
         <Link href="#projects"><button className="flex w-full gap-2 justify-center items-center bg-[#CC0000] text-white p-2 rounded-md">Ver meu trabalho <Image src="/arrow-right.svg" alt="arrow right icon" width={12} height={12}/></button></Link>
         <Link href="/"><button className="flex w-full border rounded-md justify-center gap-2 mt-3 p-1.5 text-[#2D2D2D]"><Image src="/envelope.svg" alt="envelope" width={16} height={16} />Entre em Contato</button></Link>
-        <div className="flex gap-3 mt-4 justify-center items-center">
-         <Link href="https://github.com/Hoothoot09?tab=overview&from=2025-05-01&to=2025-05-13"><button><Image src="/github-mark.svg" alt="Github icon" width={30} height={30}/></button></Link>
-         <Link href="https://www.linkedin.com/in/andrei-soares/"><button><Image src="/linkedin.svg" alt="linkedin icon" width={30} height={30}/></button></Link>
-         <Link href=""><button><Image src="" alt="" width={1} height={1}/></button></Link>
+        <div className="flex gap-5 mt-4 justify-center items-center">
+         <Link href="https://github.com/Hoothoot09?tab=overview&from=2025-05-01&to=2025-05-13"><button><Image src="/github-mark.svg" alt="Github icon" width={28} height={28}/></button></Link>
+         <Link href="https://www.linkedin.com/in/andrei-soares/"><button><Image src="/linkedin.svg" alt="linkedin icon" width={28} height={28}/></button></Link>
+         <Link href="/"><button><Image src="/envelope.svg" alt="envelope" width={30} height={30}/></button></Link>
         </div>
       </div>
     </div>

94e393230e159a7130df35f2ebefe9b0d97142f5
Andrei Soares <andreicarvalho1370@gmail.com>
2025-06-30 09:34:29 -0300

    add github button

--- my-portfolio/components/Self-introduction.tsx
+++ my-portfolio/components/Self-introduction.tsx
@@ -13,6 +13,11 @@ export default function SelfIntroduction() {
         <p className="text-[#2D2D2D] opacity-80 text-lg font-[Arial] pb-6">Sou um desenvolvedor Front-End e Designer UI/UX, estou à 1 ano meio programando, criando projetos pessoais para aprimorar minhas habilidades como programador. Procuro oportunidades para entrar no mercado de trabalho e me desenvolver profissionalmente.</p>
         <Link href="#projects"><button className="flex w-full gap-2 justify-center items-center bg-[#CC0000] text-white p-2 rounded-md">Ver meu trabalho <Image src="/arrow-right.svg" alt="arrow right icon" width={12} height={12}/></button></Link>
         <Link href="/"><button className="flex w-full border rounded-md justify-center gap-2 mt-3 p-1.5 text-[#2D2D2D]"><Image src="/envelope.svg" alt="envelope" width={16} height={16} />Entre em Contato</button></Link>
+        <div className="flex gap-2 mt-4 justify-center items-center">
+         <Link href="https://github.com/Hoothoot09?tab=overview&from=2025-05-01&to=2025-05-13"><button className=""><Image src="/github-mark.svg" alt="Github icon" width={24} height={24}/></button></Link>
+         <Link href=""><button><Image src="" alt="" width={1} height={1}/></button></Link>
+         <Link href=""><button><Image src="" alt="" width={1} height={1}/></button></Link>
+        </div>
       </div>
     </div>
   )

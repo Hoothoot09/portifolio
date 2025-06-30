9360cbc172097171449a4bdb534d54d23ffb213c
Andrei Soares <andreicarvalho1370@gmail.com>
2025-06-30 13:56:28 -0300

    add badges to the personal skills section

--- my-portfolio/components/About-me.tsx
+++ my-portfolio/components/About-me.tsx
@@ -1,3 +1,5 @@
+import {Badge} from "@/components/ui/badge";
+
 export default function AboutMe() {
   return (
     <div className="flex flex-col w-full p-3 mt-6 justify-center items-center">
@@ -12,7 +14,13 @@ export default function AboutMe() {
        <h3 className="text-2xl font-bold">Desenvolvimento Front-End</h3>
       </div>
       <div>
-       <p className="text-lg text-[#2D2D2D] opacity-80">Especialista em React, Next.js, TypeScript e frameworks modernos.</p>
+       <p className="text-lg text-[#2D2D2D] opacity-80">Especialista em React e Next.js. Aprendiz de TypeScript e Tailwind CSS.</p>
+       <div className="flex flex-wrap gap-2 mt-2">
+        <Badge className="mt-2 text-white bg-[#C50000] rounded-xl">React</Badge>
+        <Badge className="mt-2 text-[#2D2D2D] bg-[#B4A76C] rounded-xl">Next.js</Badge>
+        <Badge className="mt-2 text-white bg-[#C50000] rounded-xl">TypeScript</Badge>
+        <Badge className="mt-2 text-[#2D2D2D] bg-[#B4A76C] rounded-xl">Tailwind CSS</Badge>
+       </div>
       </div>
      </div>
     </div>

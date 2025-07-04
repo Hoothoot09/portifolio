d21891ebff541cb006836dd861d781c8c0443b24
Andrei Soares <andreicarvalho1370@gmail.com>
2025-07-04 11:56:53 -0300

    add content and styles to "Experience" section

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -1,4 +1,5 @@
 import AboutMe from "@/components/About-me";
+import Experience from "@/components/Experience";
 import Header from "@/components/Header";
 import MyProjects from "@/components/MyProjects";
 import SelfIntroduction from "@/components/Self-introduction";
@@ -15,6 +16,9 @@ export default function Home() {
       </section>
       <section>
         <MyProjects />
+      </section>
+      <section>
+        <Experience />
       </section>
     </div>
   );

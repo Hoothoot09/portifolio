9b132aff81255a3138d677928fde0c3acfe5c581
Andrei Soares <andreicarvalho1370@gmail.com>
2025-07-04 12:59:27 -0300

    add styles to the "Contact" section

--- my-portfolio/components/Contact.tsx
+++ my-portfolio/components/Contact.tsx
@@ -3,22 +3,34 @@ import Link from "next/link";
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Vamos Trabalhar Juntos!</h1>
-      <p>
+    <div className="flex flex-col w-full p-2 bg-[#E0E0E0]">
+      <h1 className="text-3xl text-center text-[#2D2D2D] font-bold mt-4 mb-4">
+        Vamos Trabalhar Juntos!
+      </h1>
+      <p className="text-xl text-[#2D2D2D] opacity-80 mb-8">
         Estou sempre interessados em novas oportunidades para me desafiar e me
         desenvolver minhas habilidades.
       </p>
-      <div>
-        <Link href={"/"}>
-          <button>
-            <Image src={"/"} alt="/" width={1} height={1} />
+      <div className="flex flex-col w-full items-center gap-3">
+        <Link href={"/"} className="w-full justify-center">
+          <button className="flex items-center bg-[#CC0000] text-white p-2 rounded-md gap-2">
+            <Image
+              src={"/envelope-white.svg"}
+              alt="white envelope icon"
+              width={20}
+              height={20}
+            />
             andreicarvalho1370@gmail.com
           </button>
         </Link>
-        <Link href={"/"}>
-          <button>
-            <Image src={"/"} alt="/" width={1} height={1} />
+        <Link href={"/"} className="w-full">
+          <button className="flex w-full justify-center items-center p-2 bg-white border border-[#B4A76C] text-[#B4A76C] rounded-md gap-2">
+            <Image
+              src={"/download-gold.svg"}
+              alt="gold download icon"
+              width={18}
+              height={18}
+            />
             Baixar Curriculo
           </button>
         </Link>

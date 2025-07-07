668f08c85774aed6d8d4cc3cdc9436a44eb46fd3
Andrei Soares <andreicarvalho1370@gmail.com>
2025-07-07 13:00:30 -0300

    add styles to the "Footer" section

--- my-portfolio/components/Footer.tsx
+++ my-portfolio/components/Footer.tsx
@@ -1,18 +1,47 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div>
-      <h2>Andrei Soares</h2>
-      <Link href={"/"}>
-        <button>github</button>
-      </Link>
-      <Link href={"/"}>
-        <button>LikedIn</button>
-      </Link>
-      <Link href={"/"}>
-        <button>Email</button>
-      </Link>
+    <div className="bg-[#2D2D2D] p-2">
+      <h2 className="text-2xl text-center text-white font-bold py-10">
+        Andrei Soares
+      </h2>
+      <div className="flex justify-center items-center gap-6">
+        <Link href={"/"}>
+          <button>
+            <Image
+              src={"/github-white.svg"}
+              alt="white github icon"
+              width={20}
+              height={20}
+            />
+          </button>
+        </Link>
+        <Link href={"/"}>
+          <button>
+            <Image
+              src={"/linkedin-white.svg"}
+              alt="white linkedin icon"
+              width={20}
+              height={20}
+            />
+          </button>
+        </Link>
+        <Link href={"/"}>
+          <button>
+            <Image
+              src={"/envelope-white.svg"}
+              alt="white envelope"
+              width={20}
+              height={20}
+            />
+          </button>
+        </Link>
+      </div>
+      <p className="text-center text-white opacity-70 mt-4 border-t border-white opacity-20 pt-8">
+        © 2025 Andrei Soares. Todos os direitos reservados.
+      </p>
     </div>
   );
 }

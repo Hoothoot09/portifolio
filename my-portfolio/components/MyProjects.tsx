768178a5dddeb5d1a54aad336abab3af2a87b334
Andrei Soares <andreicarvalho1370@gmail.com>
2026-09-23 17:33:57 -0300

    Update text and structure

--- my-portfolio/components/MyProjects.tsx
+++ my-portfolio/components/MyProjects.tsx
@@ -5,20 +5,20 @@ export default function MyProjects() {
   return (
     <div
       id="projects"
-      className="flex flex-col items-center justify-center w-full mx-auto p-4 max-sm:pt-[65px] pt-[70px] bg-[#E0E0E0]"
+      className="flex flex-col w-full p-3 pt-[70px] max-sm:mt-0 mt-6 mb-6 justify-center items-center bg-[#E0E0E0]"
     >
       <div className="mb-5">
         <h1 className="max-sm:text-2xl text-3xl text-center text-[#2D2D2D] font-[Georgia] font-bold">
           Projetos em Destaque
         </h1>
       </div>
-      <p className="max-sm:text-sm text-xl text-center text-[2D2D2D] font-[Arial] opacity-80 max-w-xl mb-8">
-        Meus projetos são desenvolvidos com foco em qualidade, desempenho e
-        experiência do usuário. Cada projeto é uma oportunidade de aplicar meus
-        conhecimentos e habilidades, buscando sempre entregar soluções
-        inovadoras e eficientes. Abaixo estão alguns dos meus projetos em
-        destaque, que demonstram minha capacidade de criar aplicações web
-        funcionais e visualmente atraentes.
+      <p className="max-sm:text-sm text-xl text-justify text-[2D2D2D] font-[Arial] opacity-80 w-full mb-8 px-35">
+        Aqui estão alguns dos meus projetos mais recentes, desenvolvidos da
+        ideia ao deploy. Cada projeto reflete minhas habilidades em
+        desenvolvimento web, design de interfaces e resolução de problemas, com
+        foco em criar experiências pensadas para o usuário e código funcional,
+        organizado e durável. Sinta-se à vontade para explorar os projetos,
+        conferir as demos ao vivo e conhecer mais sobre cada resultado.
       </p>
       <div className="flex max-lg:flex-col w-full justify-center gap-10 max-sm:mb-3 mb-6">
         <div>
